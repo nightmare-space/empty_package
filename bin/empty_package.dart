6e21c1bef595ba768c38c0af3dfcf9abbe9d6c8b
nightmare <mengyanshou@gmail.com>
2024-05-31 18:28:19 +0800

    Initial commit

---
+++ bin/empty_package.dart
@@ -0,0 +1,5 @@
+import 'package:empty_package/empty_package.dart' as empty_package;
+
+void main(List<String> arguments) {
+  print('Hello world: ${empty_package.calculate()}!');
+}
---
+++ test/empty_package_test.dart
@@ -0,0 +1,8 @@
+import 'package:empty_package/empty_package.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
