3ddb7cd61a5c4eb0d859670cba2861834ccd46df
nightmare <mengyanshou@gmail.com>
2024-07-16 21:40:58 +0800

    Add speed share extension

---
+++ speed_share_extension/bin/speed_share_extension.dart
@@ -0,0 +1,5 @@
+import 'package:speed_share_extension/speed_share_extension.dart' as speed_share_extension;
+
+void main(List<String> arguments) {
+  print('Hello world: ${speed_share_extension.calculate()}!');
+}
---
+++ speed_share_extension/test/speed_share_extension_test.dart
@@ -0,0 +1,8 @@
+import 'package:speed_share_extension/speed_share_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
